b77f7a4cdedbab423b6f3101515f1258ba917802
Gaurav Singh <gp5in9h@gmail.com>
2023-10-13 16:59:31 +0530

    adjustable list started

--- flutter-new/test_app/lib/adjustable_list.dart
+++ flutter-new/test_app/lib/adjustable_list.dart
@@ -5,6 +5,10 @@ class AdjustableList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: Text("Hello There"),
+      ),
+    );
   }
 }
