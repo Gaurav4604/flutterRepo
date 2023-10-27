78cee376a6d9079c0b2d37df6d54d051a3961412
Gaurav Singh <gp5in9h@gmail.com>
2023-10-27 19:31:07 +0530

    adjustable list

--- flutter-new/test_app/lib/adjustable_list.dart
+++ flutter-new/test_app/lib/adjustable_list.dart
@@ -5,14 +5,20 @@ class AdjustableList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: LayoutBuilder(builder: (context, constraints) {
-          return const SingleChildScrollView(
-            child: Placeholder(),
-          );
-        }),
-      ),
+    return Scaffold(
+      // required when a widget needs to be based off the Device layout
+      body: LayoutBuilder(builder: (context, constraints) {
+        return const SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Hi 1"),
+              Text("Hi 2"),
+              Text("Hi 3"),
+            ],
+          ),
+        );
+      }),
     );
   }
 }
