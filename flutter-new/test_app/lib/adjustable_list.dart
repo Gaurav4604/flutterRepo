a14967e8ad54c8b847fdd6bc55b621aaf6dcdad8
Gaurav Singh <gp5in9h@gmail.com>
2023-10-30 19:41:55 +0530

    layout builder implemented

--- flutter-new/test_app/lib/adjustable_list.dart
+++ flutter-new/test_app/lib/adjustable_list.dart
@@ -12,18 +12,47 @@ class AdjustableList extends StatelessWidget {
           child: ConstrainedBox(
               constraints: BoxConstraints(
                 minHeight: constraints.maxHeight,
+                // minWidth: constraints.maxWidth,
               ),
               child: const Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Text("Hi 1"),
-                  Text("Hi 2"),
-                  Text("Hi 3"),
+                  // Spacer(
+                  //   flex: 1,
+                  // ),
+                  ItemWidget(content: "hello there")
                 ],
               )),
         );
       }),
+    );
+  }
+}
+
+class ItemWidget extends StatelessWidget {
+  final String content;
+  const ItemWidget({super.key, required this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return ConstrainedBox(
+          constraints: BoxConstraints(
+            minWidth: constraints.maxWidth,
+          ),
+          child: Container(
+            height: 50,
+            alignment: Alignment.center,
+            decoration: const BoxDecoration(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            child: Text(content),
+          ),
+        );
+      },
     );
   }
 }
