c65cac0d62a5bf6d1a097276ce92600d8b395e24
Gaurav Singh <gp5in9h@gmail.com>
2023-11-14 16:57:07 +0530

    adaptive and dynamic layouts pt 1 done

--- flutter-new/layouts/lib/main.dart
+++ flutter-new/layouts/lib/main.dart
@@ -29,27 +29,36 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Layout Management"),
+      ),
       body: LayoutBuilder(builder: (context, constraints) {
-        if (constraints.hasBoundedHeight) {}
-
-        return Center(
-          child: ListView(
-            children: [
-              for (final person in people)
-                ListTile(
-                  title: Text(person.name),
-                  leading: Image.network(person.picture),
-                  onTap: () => Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => Scaffold(
-                          appBar: AppBar(
-                            title: Text(person.name),
-                          ),
-                          body: (PersonDetail(person))))),
-                )
-            ],
-          ),
-        );
+        if (constraints.maxWidth > 400) {
+          return const WideLayout();
+        } else {
+          return const NarrowLayout();
+        }
       }),
+    );
+  }
+}
+
+class PersonList extends StatelessWidget {
+  const PersonList({Key? key, required this.onPersonTap}) : super(key: key);
+
+  // defining a callback
+  final void Function(Person) onPersonTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        for (final person in people)
+          ListTile(
+              title: Text(person.name),
+              leading: Image.network(person.picture),
+              onTap: () => onPersonTap(person)),
+      ],
     );
   }
 }
@@ -69,6 +78,54 @@ class PersonDetail extends StatelessWidget {
           Text(person.phone),
         ],
       ),
+    );
+  }
+}
+
+class NarrowLayout extends StatelessWidget {
+  const NarrowLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: PersonList(
+        onPersonTap: (person) => Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => Scaffold(
+                appBar: AppBar(
+                  title: Text(person.name),
+                ),
+                body: (PersonDetail(person))))),
+      ),
+    );
+  }
+}
+
+class WideLayout extends StatefulWidget {
+  const WideLayout({super.key});
+
+  @override
+  State<WideLayout> createState() => _WideLayoutState();
+}
+
+class _WideLayoutState extends State<WideLayout> {
+  Person? _selectedPerson;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+            flex: 2,
+            child: PersonList(
+              onPersonTap: (person) => setState(() => _selectedPerson = person),
+            )),
+        Expanded(
+          flex: 3,
+          child: _selectedPerson == null
+              ? const Placeholder()
+              : PersonDetail(_selectedPerson!),
+        )
+      ],
     );
   }
 }
