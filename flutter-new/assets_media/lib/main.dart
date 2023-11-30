a33685900a6dfadc44c6cdfe1f4b79b7bdca8f5d
Gaurav Singh <gp5in9h@gmail.com>
2023-11-30 17:13:50 +0530

    added gif and image loading

--- flutter-new/assets_media/lib/main.dart
+++ flutter-new/assets_media/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 void main() {
   runApp(const App());
@@ -22,15 +24,47 @@ class App extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
   final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool showThisPart = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: showThisPart
+            ? [
+                Image.network('https://picsum.photos/250?image=9'),
+                Image.network(
+                    'https://docs.flutter.dev/assets/images/dash/dash-fainting.gif')
+              ]
+            : [
+                FadeInImage.memoryNetwork(
+                    placeholder: kTransparentImage,
+                    image: "https://picsum.photos/250?image=9"),
+                CachedNetworkImage(
+                  imageUrl:
+                      'https://docs.flutter.dev/assets/images/dash/dash-fainting.gif',
+                  placeholder: (_, __) => const CircularProgressIndicator(),
+                ),
+              ],
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => setState(() {
+                showThisPart = !showThisPart;
+              })),
     );
   }
 }
