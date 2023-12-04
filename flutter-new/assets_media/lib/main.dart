be852fc9e5b9a11cee4deb1c5ab2847b1f8cbeca
Gaurav Singh <gp5in9h@gmail.com>
2023-12-04 11:58:44 +0530

    video component added

--- flutter-new/assets_media/lib/main.dart
+++ flutter-new/assets_media/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:transparent_image/transparent_image.dart';
+import 'package:video_player/video_player.dart';
 
 void main() {
   runApp(const App());
@@ -34,37 +35,88 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   bool showThisPart = false;
+  late VideoPlayerController _controller;
+  late Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = VideoPlayerController.networkUrl(Uri.parse(
+      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+    ));
+
+    _initializeVideoPlayerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: showThisPart
-            ? [
-                Image.network('https://picsum.photos/250?image=9'),
-                Image.network(
-                    'https://docs.flutter.dev/assets/images/dash/dash-fainting.gif')
-              ]
-            : [
-                FadeInImage.memoryNetwork(
-                    placeholder: kTransparentImage,
-                    image: "https://picsum.photos/250?image=9"),
-                CachedNetworkImage(
-                  imageUrl:
-                      'https://docs.flutter.dev/assets/images/dash/dash-fainting.gif',
-                  placeholder: (__, ___) => const CircularProgressIndicator(),
-                ),
-              ],
-      ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () => setState(() {
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: showThisPart
+              ? [
+                  FutureBuilder(
+                    future: _initializeVideoPlayerFuture,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        return AspectRatio(
+                          aspectRatio: _controller.value.aspectRatio,
+                          child: VideoPlayer(_controller),
+                        );
+                      } else {
+                        return const CircularProgressIndicator();
+                      }
+                    },
+                  ),
+                  Image.network('https://picsum.photos/250?image=9'),
+                  Image.network(
+                      'https://docs.flutter.dev/assets/images/dash/dash-fainting.gif')
+                ]
+              : [
+                  FadeInImage.memoryNetwork(
+                      placeholder: kTransparentImage,
+                      image: "https://picsum.photos/250?image=9"),
+                  CachedNetworkImage(
+                    imageUrl:
+                        'https://docs.flutter.dev/assets/images/dash/dash-fainting.gif',
+                    placeholder: (__, ___) => const CircularProgressIndicator(),
+                  ),
+                ],
+        ),
+        floatingActionButton: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            TextButton(
+              onPressed: () => setState(() {
                 showThisPart = !showThisPart;
-              })),
-    );
+              }),
+              child: const Text("Change"),
+            ),
+            IconButton(
+              onPressed: () => setState(() {
+                if (_controller.value.isPlaying) {
+                  _controller.pause();
+                } else {
+                  _controller.play();
+                }
+              }),
+              icon: Icon(_controller.value.isPlaying
+                  ? Icons.pause_sharp
+                  : Icons.play_arrow),
+            )
+          ],
+        ));
   }
 }
