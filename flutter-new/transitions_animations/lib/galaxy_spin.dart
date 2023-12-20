9132ef99ebe1549c4d601ee969878f7b443508d4
Gaurav Singh <gp5in9h@gmail.com>
2023-12-20 16:35:29 +0530

    added control over explicit animation controller

--- flutter-new/transitions_animations/lib/galaxy_spin.dart
+++ flutter-new/transitions_animations/lib/galaxy_spin.dart
@@ -36,16 +36,50 @@ class _GalaxySpinState extends State<GalaxySpin>
         body: Theme(
           data: ThemeData(
               colorScheme: const ColorScheme.dark(background: Colors.black)),
-          child: RotationTransition(
-            turns: _controller,
-            alignment: Alignment.center,
-            child: Image.asset(
-              "assets/galaxy_transparent.png",
-              height: 400,
-              color: Colors.white,
-            ),
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment.topCenter,
+                child: RotationTransition(
+                  turns: _controller,
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                    "assets/galaxy_transparent.png",
+                    height: 400,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: TimeStopper(controller: _controller),
+              )
+            ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class TimeStopper extends StatelessWidget {
+  final AnimationController controller;
+  const TimeStopper({super.key, required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (controller.isAnimating) {
+          controller.stop();
+        } else {
+          controller.repeat();
+        }
+      },
+      child: Container(
+        color: Colors.transparent,
+        width: 300,
+        height: 300,
       ),
     );
   }
