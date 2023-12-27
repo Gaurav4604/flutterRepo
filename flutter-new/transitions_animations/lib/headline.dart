ec8e0472835363e484df6e1de34d5cdf3a057239
Gaurav Singh <gp5in9h@gmail.com>
2023-12-28 00:26:40 +0530

    fixed widget inheritance

--- flutter-new/transitions_animations/lib/headline.dart
+++ flutter-new/transitions_animations/lib/headline.dart
@@ -1,50 +1,48 @@
 import 'package:flutter/material.dart';
 
+const List<Color> headlineTextColors = [Colors.green, Colors.red];
+const Duration headlineAnimationDuration = Duration(milliseconds: 400);
+
 class Headline extends ImplicitlyAnimatedWidget {
   final String text;
   final int index;
-  const Headline({Key? key, required this.text, required this.index})
-      : super(
-            key: key,
-            duration: const Duration(seconds: 1),
-            curve: Curves.easeIn);
 
-  Color get targetColor => index == 0 ? Colors.red : Colors.green;
+  const Headline({Key? key, required this.text, required this.index})
+      : super(duration: headlineAnimationDuration);
+
+  Color get targetColor => headlineTextColors[index];
 
   @override
   HeadlineState createState() => HeadlineState();
 }
 
-class HeadlineState extends ImplicitlyAnimatedWidgetState<Headline> {
-  ColorTween? _colorTween;
-
-  @override
-  void initState() {
-    animation.addStatusListener((status) => print(status));
-    super.initState();
-  }
+class HeadlineState extends AnimatedWidgetBaseState<Headline> {
+  GhostFadeTween? _colorTween;
 
   @override
   void forEachTween(TweenVisitor<dynamic> visitor) {
-    _colorTween = visitor(_colorTween, widget.targetColor,
-        (dynamic color) => ColorTween(begin: color)) as ColorTween;
+    _colorTween = visitor(
+      _colorTween,
+      widget.targetColor,
+      (color) => GhostFadeTween(begin: color),
+    ) as GhostFadeTween?;
   }
 
   @override
   Widget build(BuildContext context) {
     return Text(
-      '$widget.text',
-      style: TextStyle(color: _colorTween?.evaluate(animation)),
+      widget.text,
+      style: TextStyle(color: _colorTween!.evaluate(animation)),
     );
   }
 }
 
-class GhostFadeTween extends Tween<Color> {
+class GhostFadeTween extends Tween<Color?> {
   final Color middle = Colors.white;
 
   GhostFadeTween({Color? begin, Color? end}) : super(begin: begin, end: end);
 
-  Color lerp(double t) {
+  Color? lerp(double t) {
     if (t < 0.5) {
       return Color.lerp(begin, middle, t * 2);
     } else {
