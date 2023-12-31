f100ad63053ebf9a770dd9a1ce8ea71a910b075a
Gaurav Singh <gp5in9h@gmail.com>
2023-12-31 17:45:24 +0530

    started with hero animations

---
+++ flutter-new/transitions_animations/lib/hero_animation_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HeroAnimationScreen extends StatelessWidget {
+  const HeroAnimationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Hero Animation"),
+      ),
+    );
+  }
+}
