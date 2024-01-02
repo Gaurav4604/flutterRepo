2363cb7f44e1ec2ac52eb421d95d3ba638b7aa42
Gaurav Singh <gp5in9h@gmail.com>
2024-01-02 18:39:55 +0530

    started with radial transitions

---
+++ flutter-new/transitions_animations/lib/radial_animation_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RadialAnimationScreen extends StatelessWidget {
+  const RadialAnimationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Radial Animation Initial"),
+      ),
+      body: const Center(),
+    );
+  }
+}
