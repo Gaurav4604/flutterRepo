f282f0c88fe520b35ba8f5e9270c8f4a9cba51ff
Gaurav Singh <gp5in9h@gmail.com>
2024-01-06 19:12:42 +0530

    started with stagger

---
+++ flutter-new/transitions_animations/lib/stagger_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StaggerScreen extends StatelessWidget {
+  const StaggerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
