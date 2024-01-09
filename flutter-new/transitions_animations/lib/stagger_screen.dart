4f792fea7f5a86bb9e81fe3a33ec8ffe54571065
Gaurav Singh <gp5in9h@gmail.com>
2024-01-09 20:02:50 +0530

    getting started with menu items for the stagger list

--- flutter-new/transitions_animations/lib/stagger_screen.dart
+++ flutter-new/transitions_animations/lib/stagger_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/scheduler/ticker.dart';
 
 class StaggerScreen extends StatelessWidget {
   const StaggerScreen({super.key});
@@ -10,5 +11,41 @@ class StaggerScreen extends StatelessWidget {
         child: Text("Animation Staggered"),
       ),
     );
+  }
+}
+
+class Menu extends StatefulWidget {
+  const Menu({super.key});
+
+  @override
+  State<Menu> createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+  static const _menuTitles = [
+    'Declarative Style',
+    'Premade Widgets',
+    'Stateful Hot Reload',
+    'Native Performance',
+    'Great Community',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Stack(
+        fit: StackFit.expand,
+        children: [_buildFlutterLogo(), _buildContent()],
+      ),
+    );
+  }
+
+  Widget _buildFlutterLogo() {
+    return const Placeholder();
+  }
+
+  Widget _buildContent() {
+    return const Placeholder();
   }
 }
